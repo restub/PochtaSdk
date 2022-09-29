6322c7c176c87fa4e5bbd55f02768762bd154979
yallie <yallie@yandex.ru>
2022-09-29 16:31:58 +0300

    Improved the diagnostics.

--- PochtaSdk/Toolbox/TariffDateTimeConverter.cs
+++ PochtaSdk/Toolbox/TariffDateTimeConverter.cs
@@ -1,4 +1,8 @@
-﻿using Newtonsoft.Json.Converters;
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Restub;
 
 namespace PochtaSdk.Toolbox
 {
@@ -7,6 +11,19 @@ namespace PochtaSdk.Toolbox
         public TariffDateTimeConverter()
         {
             DateTimeFormat = "yyyyMMdd\\THHmmss";
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            try
+            {
+                return base.ReadJson(reader, objectType, existingValue, serializer);
+            }
+            catch (FormatException ex)
+            {
+                throw new RestubException(HttpStatusCode.OK, "Cannot deserialize " +
+                    $"DateTime value of '{reader.Value}': {ex.Message}", ex);
+            }
         }
     }
 }
